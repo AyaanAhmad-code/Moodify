c4fc0fa624f904e6c7b7286263d7e438a29d0121
Ayaan Ahmad <ayaanah287@gmail.com>
2026-03-12 03:48:49 +0530

    /

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "https://moodify-bice-psi.vercel.app/",
+    origin: "https://moodify-bice-psi.vercel.app",
     credentials: true
 }));
 
